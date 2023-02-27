f6fd851fba3149ce1b80a974937386df19b64830
Firenoo <49818773+firenoo@users.noreply.github.com>
2023-02-27 09:57:24 -0800

    Add a texture for the resize button.
    Add CN localization for tooltip

--- src/main/java/com/projecturanus/betterp2p/client/gui/widget/GuiScale.kt
+++ src/main/java/com/projecturanus/betterp2p/client/gui/widget/GuiScale.kt
@@ -1,8 +1,8 @@
 package com.projecturanus.betterp2p.client.gui.widget
 
-enum class GuiScale(inline val size: (Int) -> Int, val minHeight: Int) {
-    SMALL({ 4 }, 0),
-    NORMAL({ 6 }, 326),
-    LARGE({ 8 }, 409),
-    DYNAMIC({ availableHeight -> availableHeight / P2P_ENTRY_HEIGHT }, 0)
+enum class GuiScale(inline val size: (Int) -> Int, val minHeight: Int, val unlocalizedName: String) {
+    SMALL({ 4 }, 0, "gui.advanced_memory_card.gui_scale.small"),
+    NORMAL({ 6 }, 326, "gui.advanced_memory_card.gui_scale.normal"),
+    LARGE({ 8 }, 409, "gui.advanced_memory_card.gui_scale.large"),
+    DYNAMIC({ availableHeight -> availableHeight / P2P_ENTRY_HEIGHT }, 0, "gui.advanced_memory_card.gui_scale.dynamic")
 }
--- src/main/java/com/projecturanus/betterp2p/client/gui/widget/WidgetButton.kt
+++ src/main/java/com/projecturanus/betterp2p/client/gui/widget/WidgetButton.kt
@@ -1,6 +1,14 @@
 package com.projecturanus.betterp2p.client.gui.widget
 
+import com.projecturanus.betterp2p.MODID
+import com.projecturanus.betterp2p.client.gui.GUI_TEX_HEIGHT
+import com.projecturanus.betterp2p.client.gui.GUI_WIDTH
+import com.projecturanus.betterp2p.client.gui.GuiAdvancedMemoryCard
+import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.GuiButton
+import net.minecraft.client.renderer.OpenGlHelper
+import net.minecraft.client.renderer.Tessellator
+import org.lwjgl.opengl.GL11
 
 /**
  * Widget button for stuff.
@@ -10,18 +18,44 @@ import net.minecraft.client.gui.GuiButton
  * @param height
  * @param hoverText - unlocalized text to display when mouse hovering
  */
-abstract class WidgetButton(x: Int, y: Int, private var width: Int, private var height: Int,
+abstract class WidgetButton(val gui: GuiAdvancedMemoryCard, x: Int, y: Int, width: Int, height: Int,
                             val hoverText: () -> String): GuiButton(0, x, y, width, height, "") {
+
+    private var texX = 0.0
+    private var texY = 0.0
 
     abstract fun mousePressed(mouseX: Int, mouseY: Int)
 
-    fun draw(mouseX: Int, mouseY: Int, partial: Float) {
-        drawRect(xPosition, yPosition, xPosition + width, yPosition + height, 0xFF shl 24)
+    fun draw(mc: Minecraft?, mouseX: Int, mouseY: Int, partialTicks: Float) {
+        val tessellator = Tessellator.instance
+        gui.bindTexture(MODID, "textures/gui/advanced_memory_card.png")
+        GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f)
+        field_146123_n = mouseX >= xPosition && mouseY >= yPosition && mouseX < xPosition + width && mouseY < yPosition + height
+        val k = getHoverState(field_146123_n)
+        GL11.glEnable(GL11.GL_BLEND)
+        OpenGlHelper.glBlendFunc(770, 771, 1, 0)
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA)
+        tessellator.startDrawingQuads()
+        tessellator.addVertexWithUV(xPosition.toDouble(), (yPosition + height).toDouble(), 0.0, (32.0 * k) / GUI_WIDTH, (232.0 + height) / GUI_TEX_HEIGHT)
+        tessellator.addVertexWithUV(((xPosition + width).toDouble()), (yPosition + height).toDouble(), 0.0, (32.0 * (k + 1)) / GUI_WIDTH, (232.0 + height) / GUI_TEX_HEIGHT)
+        tessellator.addVertexWithUV((xPosition + width).toDouble(), yPosition.toDouble(), 0.0, (32.0 * (k + 1)) / GUI_WIDTH, 232.0 / GUI_TEX_HEIGHT)
+        tessellator.addVertexWithUV(xPosition.toDouble(), yPosition.toDouble(), 0.0, (32.0 * k) / GUI_WIDTH, 232.0 / GUI_TEX_HEIGHT)
+        tessellator.draw()
+        tessellator.startDrawingQuads()
+        tessellator.addVertexWithUV(xPosition.toDouble(), (yPosition + height).toDouble(), 0.0, texX / GUI_WIDTH, (texY + height) / GUI_TEX_HEIGHT)
+        tessellator.addVertexWithUV(((xPosition + width).toDouble()), (yPosition + height).toDouble(), 0.0, (texX + width) / GUI_WIDTH, (texY + height) / GUI_TEX_HEIGHT)
+        tessellator.addVertexWithUV((xPosition + width).toDouble(), yPosition.toDouble(), 0.0, (texX + width) / GUI_WIDTH, texY / GUI_TEX_HEIGHT)
+        tessellator.addVertexWithUV(xPosition.toDouble(), yPosition.toDouble(), 0.0, texX / GUI_WIDTH, texY / GUI_TEX_HEIGHT)
+        tessellator.draw()
     }
-
     fun setPosition(x: Int, y: Int) {
         this.xPosition = x
         this.yPosition = y
+    }
+
+    fun setTexCoords(x: Double, y: Double) {
+        this.texX = x
+        this.texY = y
     }
 
     fun setSize(width: Int, height: Int) {
